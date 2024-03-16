889f1490ce40eef5c343574577e72c77e275074c
Matias <matiases1995@outlook.es>
2024-03-16 15:22:43 -0300

    mod statistics

--- src/components/ContentWrap.jsx
+++ src/components/ContentWrap.jsx
@@ -4,32 +4,32 @@ import NextReleases from "./NextReleases"
 import MostPopular from "./MostPopular"
 import Genres from "./Genres"
 
-// const data = [
-//     {
-//         titulo: "Cantidad de estrenos",
-//         cifra: "15",
-//         icono: "bi bi-film",
-//         colorIcono: "cornflowerblue"
-//     },
-//     {
-//         titulo: "Categorias",
-//         cifra: "12",
-//         icono: "bi bi-tags-fill",
-//         colorIcono: "orange"
-//     },
-//     {
-//         titulo: "Total Ventas",
-//         cifra: "$ 489.567",
-//         icono: "bi bi-currency-dollar",
-//         colorIcono: "green"
-//     }
-// ];
+const data = [
+    {
+        titulo: "Cantidad de estrenos",
+        cifra: "15",
+        icono: "bi bi-film",
+        colorIcono: "cornflowerblue"
+    },
+    {
+        titulo: "Categorias",
+        cifra: "12",
+        icono: "bi bi-tags-fill",
+        colorIcono: "orange"
+    },
+    {
+        titulo: "Total Ventas",
+        cifra: "$ 489.567",
+        icono: "bi bi-currency-dollar",
+        colorIcono: "green"
+    }
+];
 
 function ContentWrap(){
     return (
         <main className="content-wrap">
 			<PreSale />
-			<Statistics />
+			<Statistics info = { data }/>
             <NextReleases />
 			<MostPopular />
             <Genres />
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,52 +1,24 @@
-function Statistics() {
+export default function Statistics2(props) {
   return (
     <section className="content">
-      <h2 className="mt-3">Estadísticas</h2>
+      <h2 className="mt-3">Estadisticas</h2>
       <div className="info-boxes">
-        <div className="info-box">
-          <div className="box-icon">
-            <i
-              className="bi bi-film"
-              style={{ fontSize: "2.5rem", color: "cornflowerblue" }}
-            ></i>
-          </div>
+        {props.info.map((elemento, i) => {
+            return <div className="info-box" key={elemento + i}>
+                        <div className="box-icon">
+                            <i
+                                className={elemento.icono}
+                                style={{ fontSize: "2.5rem", color: elemento.colorIcono }}
+                            ></i>
+                        </div>
 
-          <div className="box-content">
-            <span className="big">15</span>
-            Cantidad de estrenos
-          </div>
-        </div>
-
-        <div className="info-box">
-          <div className="box-icon">
-            <i
-              className="bi bi-tags-fill"
-              style={{ fontSize: "2.5rem", color: "orange" }}
-            ></i>
-          </div>
-
-          <div className="box-content">
-            <span className="big">12</span>
-            Categorías
-          </div>
-        </div>
-
-        <div className="info-box active">
-          <div className="box-icon">
-            <i
-              className="bi bi-currency-dollar"
-              style={{ fontSize: "2.5rem", color: "green" }}
-            ></i>
-          </div>
-
-          <div className="box-content">
-            <span className="big">$ 489.567</span>
-            Total ventas
-          </div>
-        </div>
+                        <div className="box-content">
+                            <span className="big">{elemento.cifra}</span>
+                            {elemento.titulo}
+                        </div>
+                    </div>;
+        })}
       </div>
     </section>
   );
 }
-
-export default Statistics;
